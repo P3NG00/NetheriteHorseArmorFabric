68aebd8ebd1c78808f0537cc062a4a5a9a8f58fa
P3NG00 <p3ng00stuff@gmail.com>
2020-07-10 20:41:19 -0600

    0.1.0

--- src/main/java/com/p3ng00/netheritehorsearmor/NetheriteHorseArmor.java
+++ src/main/java/com/p3ng00/netheritehorsearmor/NetheriteHorseArmor.java
@@ -1,21 +1,33 @@
 package com.p3ng00.netheritehorsearmor;
 
+import com.p3ng00.netheritehorsearmor.item.HorseArmorItem;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.item.HorseArmorItem;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.loot.BinomialLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class NetheriteHorseArmor implements ModInitializer {
 
+    public static final String MODID = "netheritehorsearmor"; // Mod ID
+
+    public static final Item NETHERITE_HORSE_ARMOR = new HorseArmorItem(15, "netherite", new Item.Settings().maxCount(1).group(ItemGroup.MISC).fireproof());
+
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("netheritehorsearmor", "netherite_horse_armor"), new HorseArmorItem(15, "netherite", new Item.Settings().maxCount(1).group(ItemGroup.MISC).fireproof()) {
-            @Override
-            public Identifier getEntityTexture() {
-                return new Identifier("netheritehorsearmor", super.getEntityTexture().getPath());
+
+        // Register Item
+        Registry.register(Registry.ITEM, new Identifier(MODID, "netherite_horse_armor"), NETHERITE_HORSE_ARMOR);
+
+        // Add Item to Bastion Treasure loot table
+        LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, identifier, fabricLootSupplierBuilder, lootTableSetter) -> {
+            if (identifier.toString().equals("minecraft:chests/bastion_treasure")) {
+                fabricLootSupplierBuilder.withPool(FabricLootPoolBuilder.builder().rolls(BinomialLootTableRange.create(3, 0.25f)).withEntry(ItemEntry.builder(NETHERITE_HORSE_ARMOR).build()).build());
             }
-        });
+        }));
     }
 }
---
+++ src/main/java/com/p3ng00/netheritehorsearmor/item/HorseArmorItem.java
@@ -0,0 +1,19 @@
+package com.p3ng00.netheritehorsearmor.item;
+
+import com.p3ng00.netheritehorsearmor.NetheriteHorseArmor;
+import net.minecraft.util.Identifier;
+
+public class HorseArmorItem extends net.minecraft.item.HorseArmorItem {
+
+    private final Identifier texture;
+
+    public HorseArmorItem(int bonus, String name, Settings settings) {
+        super(bonus, name, settings);
+        texture = new Identifier(NetheriteHorseArmor.MODID, getEntityTexture().getPath());
+    }
+
+    @Override
+    public Identifier getEntityTexture() {
+        return texture;
+    }
+}
